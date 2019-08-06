37a318b86de4677a6d25114253cf85fb7c07ca84
Veselin Nikolov <veselin@automattic.com>
2019-08-06 15:54:31 +0300

    QuerySitePlans and QueryPlans are needed for users who visited the cancelation without visiting plans first

--- client/components/marketing-survey/cancel-purchase-form/index.jsx
+++ client/components/marketing-survey/cancel-purchase-form/index.jsx
@@ -49,6 +49,8 @@ import stepsForProductAndSurvey from './steps-for-product-and-survey';
 import enrichedSurveyData from './enriched-survey-data';
 import { CANCEL_FLOW_TYPE } from './constants';
 import { getIncludedDomainPurchase, getDowngradePlanRawPrice } from 'state/purchases/selectors';
+import QueryPlans from 'components/data/query-plans';
+import QuerySitePlans from 'components/data/query-site-plans';
 
 /**
  * Style dependencies
@@ -671,6 +673,7 @@ class CancelPurchaseForm extends React.Component {
 	}
 
 	render() {
+		const { selectedSite } = this.props;
 		return (
 			<Dialog
 				isVisible={ this.props.isVisible }
@@ -679,6 +682,8 @@ class CancelPurchaseForm extends React.Component {
 				className="cancel-purchase-form__dialog"
 			>
 				{ this.surveyContent() }
+				<QueryPlans />
+				{ selectedSite && <QuerySitePlans siteId={ selectedSite.ID } /> }
 			</Dialog>
 		);
 	}
