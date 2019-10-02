82c764509d1ac6afa42afc8b3b421e9fe3336bef
Jon Surrell <jon.surrell@automattic.com>
2019-10-02 20:27:48 +0200

    Add types to treeSelect

--- packages/tree-select/src/index.ts
+++ packages/tree-select/src/index.ts
@@ -1,5 +1,3 @@
-/** @format */
-
 const defaultGetCacheKey = ( ...args ) => args.join();
 
 const isFunction = fn => {
@@ -11,17 +9,58 @@ const isObject = o => {
 };
 
 /**
+ * Generics
+ *
+ * S: State - the application state
+ * D: Dependents - Array of dependent values returned by the DependentsSelector
+ * R: Result of Selector
+ * O: Other parameters that the computation and resulting CachedSelector are provided
+ */
+
+/**
+ * DependentsSelector is a function that accepts a State (S) object and
+ * returns an array of values to be used in the Selector as well
+ * as the values used by the caching/memoization layer.
+ */
+type DependentsSelector< S, O, D > = ( S, ...O ) => D & any[];
+
+/**
+ * Function that computes a value based on the dependent values provided
+ * by the DependentsSelector. It receives the values returned by
+ * DependentsSelector as its first argument, the rest of the arguments
+ * given to the computation are the same as the CachedSelector retured
+ * by treeSelect.
+ */
+type Selector< D, R, O > = ( D, ...O ) => R;
+
+/**
+ * The cached selector is the returned function from treeSelect. It should
+ * have the same signatrue as Selector except it accepts the State as its
+ * first argument instead of the result of DependentsSelector. The rest of
+ * the other (O) arguments are the same provided to the Selector.
+ */
+type CachedSelector< S, R, O > = ( S, ...O ) => R;
+
+interface Options< O > {
+	/**
+	 * Custom function to compute the cache key from the selector's `args` list
+	 */
+	getCacheKey?: ( ...O ) => string;
+}
+
+/**
  * Returns a selector that caches values.
  *
- * @param  {Function} getDependents A Function describing the dependent(s) of the selector.
- *                                    Must return an array which gets passed as the first arg to the selector
- * @param  {Function} selector      A standard selector for calculating cached result
- * @param  {Object}   options       Options bag with additional arguments
- * @param  {Function} options.getCacheKey
- *                                  Custom way to compute the cache key from the `args` list
- * @return {Function}               Cached selector
+ * @param  getDependents A Function describing the dependent(s) of the selector. Must return an array which gets passed as the first arg to the selector.
+ * @param  selector      A standard selector for calculating cached result.
+ * @param  options       Additional options
+ * @return               Cached selector
  */
-export default function treeSelect( getDependents, selector, options = {} ) {
+export default function treeSelect< S, D, R, O >(
+	getDependents: DependentsSelector< S, O, D >,
+	selector: Selector< D, R, O >,
+	options: Options< O > = {}
+): CachedSelector< S, R, O > {
 	if ( process.env.NODE_ENV !== 'production' ) {
 		if ( ! isFunction( getDependents ) || ! isFunction( selector ) ) {
 			throw new TypeError(
